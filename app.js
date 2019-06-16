f2bcd3e6b77df8ad3bd18e5052e953d938ed258d
Carlos Ueda <carlos.ueda@gmail.com>
2019-06-16 05:22:45 -0300

    Form created. Closes #1

--- app.js
+++ app.js
@@ -1,9 +1,37 @@
-const user = {
-  firstName: 'John',
-  lastName: 'Doe',
-  age: 40
-};
+//Define UI vars
+const form = document.querySelector("#task-form");
+const taskList = document.querySelector(".collection");
+const clearBtn = document.querySelector(".clear-tasks");
+const filter = document.querySelector("#filter");
+const taskInput = document.querySelector("#task");
 
-for(let x in user){
-  console.log(user[x]);
+// Load all event listeners
+loadEventListeners();
+
+function loadEventListeners(){
+  // Add task event
+  form.addEventListener('submit', addTask);
+}
+// Add task
+function addTask(e){
+  if(taskInput.value === ''){
+    alert('Add a Task');
+  }
+
+  // Create li element
+  const li = document.createElement('li');
+  // Add class
+  li.className = 'collection-item';
+  li.appendChild(document.createTextNode(taskInput.value));
+  // Create new link element
+  const link = document.createElement('a');
+  link.className = 'delete-item secondary-content';
+  //Add icon html
+  link.innerHTML = '<i class="fa fa-remove"></i>';
+  //Append the link to li
+  li.appendChild(link);
+  //Append li to ul
+  taskList.appendChild(li);
+  
+  e.preventDefault();
 }
